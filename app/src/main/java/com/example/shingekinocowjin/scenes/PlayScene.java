800fcd8179a75a6ef7924c81fef076463d30d795
Anna Nguyen <anguyen373@gatech.edu>
2022-03-31 01:56:48 -0400

    Added new welcome screen background, added title to welcome screen

--- app/src/main/java/com/example/shingekinocowjin/scenes/PlayScene.java
+++ app/src/main/java/com/example/shingekinocowjin/scenes/PlayScene.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.view.MotionEvent;
+import android.view.View;
 
 import com.example.shingekinocowjin.GameState;
 import com.example.shingekinocowjin.Player;
